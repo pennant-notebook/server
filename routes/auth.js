7d323269c9bf6d823bad52ded88533737de2762d
Marwan Zaarab <marwan.zaarab@gmail.com>
2023-09-01 15:25:31 -0500

    using HttpOnly cookies for storing tokens

--- routes/auth.js
+++ routes/auth.js
@@ -104,6 +104,7 @@ router.get('/getAccessToken', async (req, res) => {
     })
     .then(data => {
       res.cookie('pennantAccessToken', data.access_token, {
+        httpOnly: true,
         sameSite: 'lax',
         secure: process.env.NODE_ENV === 'production'
       });

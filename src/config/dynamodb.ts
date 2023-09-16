8e7921da878a18c4c144654ff77525fd26fb03f9
Marwan Zaarab <marwan.zaarab@gmail.com>
2023-09-16 08:14:07 -0500

    Converted the server to TypeScript

--- src/config/dynamodb.ts
+++ src/config/dynamodb.ts
@@ -2,7 +2,10 @@ import 'dotenv/config';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
-// Configuration for DynamoDB
+if (!process.env.DYNAMO_AWS_ACCESS_KEY_ID || !process.env.DYNAMO_AWS_SECRET_ACCESS_KEY) {
+  throw new Error('AWS credentials are not set in the environment variables');
+}
+
 export const dynamoClient = new DynamoDBClient({
   region: 'us-east-1',
   credentials: {
@@ -14,11 +17,3 @@ export const dynamoClient = new DynamoDBClient({
 const dynamodb = DynamoDBDocument.from(dynamoClient);
 
 export default dynamodb;
-// Configuration for S3
-// const s3Config = {
-//   region: process.env.S3_AWS_REGION,
-//   accessKeyId: process.env.S3_AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.S3_AWS_SECRET_ACCESS_KEY
-// };
-
-// Create new instances of AWS.S3 and AWS.DynamoDB with the respective configurations

a1a07a0c6b017fc7495ef30c62eb8b0ddb5f43b2
Marwan Zaarab <marwan.zaarab@gmail.com>
2023-10-01 14:35:04 -0400

    change module to commonjs and serve dist from client

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,32 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 import { authRouter, apiRouter } from './routes';
-
-
 
 const app = express();
 
 app.use(cors());
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Set-Cookie', ['SameSite=Strict;Secure;Path=/']);
   next();
 });
 
 app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 if (process.env.NODE_ENV !== 'development') {
-  app.use(express.static(path.join(__dirname, '../dist')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
 }
 
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
 
-app.post('/api/hocuspocus', (req, res) => {
+app.post('/api/hocuspocus', (req: Request, res: Response) => {
   const event = req.body.event;
   const payload = req.body.payload;
 
@@ -40,7 +38,7 @@ app.post('/api/hocuspocus', (req, res) => {
 
 if (process.env.NODE_ENV !== 'development') {
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
 
